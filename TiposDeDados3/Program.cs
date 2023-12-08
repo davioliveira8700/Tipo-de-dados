a1dbe135b8a357a1a2886f9f65cc20cfe374949b
Davi de Oliveira <odavi278@gmail.com>
2023-12-07 23:42:41 -0300

    Tipos de dados boolean e char

---
+++ TiposDeDados3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Atribuindo valores para o tipo boolean e char");
+
+bool ativo = true;
+Boolean inativo = false;
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+
+Console.WriteLine(x > 15);
+
+char letra1 = 'A';
+Console.WriteLine(letra1);
+
+Console.ReadLine();
