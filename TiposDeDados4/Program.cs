1dcc77b0613c3c0e8b0665470cc0b34ac6fc5677
Davi de Oliveira <odavi278@gmail.com>
2023-12-07 23:54:48 -0300

    Dados do tipo string

---
+++ TiposDeDados4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Atribuindo valores a variaveis tipo string");
+Console.WriteLine();
+
+string nome = "Curso C#";
+String titulo = "Curso C# essencial";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+Console.ReadLine();
+
+object nota = 10;
+object valor = 8.55m;
+object nome2 = "Maria";
+object ativa = true;
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor);
+Console.WriteLine(nome2);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+  
+
