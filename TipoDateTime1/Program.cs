62420dd2b49da24f6feb910352ad941f84c530fb
Davi de Oliveira <odavi278@gmail.com>
2023-12-08 00:47:54 -0300

    Tipo data time

---
+++ TipoDateTime1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("## Struct DateTime");
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+//criar uma data especifica usa formato: aaa, mm, dd
+DateTime dataHoje = new DateTime(2022, 09, 09);
+Console.WriteLine(dataHoje);
+
+//definir as horas 
+DateTime dataHoraHoje = new DateTime(2022, 09, 06 ,21, 10, 30);
+Console.WriteLine(dataHoraHoje);
+
+Console.ReadKey();
