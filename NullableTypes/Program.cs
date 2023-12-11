48dc892d54e2ee8028dde4e6672acbbb9f5841cf
Davi de Oliveira <odavi278@gmail.com>
2023-12-11 20:25:08 -0300

    Introdução a nullable type

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Nullable types");
+
+Nullable<int> i = null;
+Nullable<double> d = null;
+Nullable<bool> b = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(b);
+
+//Sintaxe alternativa
+
+int? a = null;
+double? c = null;
+bool? n = null;
+Console.WriteLine(a);
+Console.WriteLine(c);
+Console.WriteLine(n);
+
+int? z = 100;
+if (z.HasValue)
+{
+    Console.WriteLine($"z = {z.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
+
+
+Console.ReadKey();
