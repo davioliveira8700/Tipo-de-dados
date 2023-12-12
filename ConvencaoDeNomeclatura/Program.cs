3dace03879c05db34645a510a55bb375ad9d2744
Davi de Oliveira <odavi278@gmail.com>
2023-12-12 20:49:51 -0300

    convecao de nomeclatura

---
+++ ConvencaoDeNomeclatura/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Definindo indentificadores");
+
+Console.WriteLine();
+
+//identificadores validos
+string nome;
+string nomeCompleto;
+int idade;
+int idade1;
+int valor;
+
+//identificadores invalidos
+//int 5idades;
+//int $valor;
+//int valor#total;
+
+// para nomes de variaveis: camel case (camelCase)
+string descontoTotal;
+string desconto_Total;
+
+// constantes: maiusculas (MAIUSCULAS)
+
+double PI = 3.14;
+string PREFIXO = "11";
+string PREFIXO_SP = "11";
+
+
+
+// para nomes de classes: pascal case (PascalCase)
+class ImprimirTexto
+{
+    public void imprimeNome()
+    {
+        Console.WriteLine("Davi");
+    }
+}
+
+
