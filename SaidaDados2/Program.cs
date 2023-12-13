2d77fa1f7aa6a367a766bc2f5d5057e3d8116769
Davi de Oliveira <odavi278@gmail.com>
2023-12-13 00:03:37 -0300

    saida de dados com escape

---
+++ SaidaDados2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Saida de dados: Usando a sequencia de escapes");
+Console.WriteLine();
+
+String pizza = "Pizza \tde \tmussarela";
+Console.WriteLine(pizza);
+string bolo = "\nbolo \nde \nchocolate";
+Console.WriteLine(bolo);
+
+Console.Read();
