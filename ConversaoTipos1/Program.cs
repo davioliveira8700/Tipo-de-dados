5a079ae98baf068e53471c953f1773ef8ea2af0b
Davi de Oliveira <odavi278@gmail.com>
2023-12-19 01:31:16 -0300

    Entrada de dados

---
+++ ConversaoTipos1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Conversão de tipos \n");
+
+double varDouble = 12.456; // 8 bytes
+int varInt = (int)varDouble;
+
+Console.WriteLine(varInt);
+
+int num1 = 10;
+int num2 = 4;
+
+double resultado = (double)num1 / num2;
+
+Console.WriteLine(resultado);
+
+
+
+Console.ReadLine();
---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Entrada de Dados");
+
+Console.WriteLine("\nInforme seu nome");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nO seu nome é " + nome);
+Console.WriteLine($"\n O seu nome é {nome}");
+
+Console.WriteLine("\n Informe sua idade");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nIdade " + idade + " anos");
+
+Console.ReadKey();
