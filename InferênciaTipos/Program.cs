ca1fbcec7614cbae06f700574c0b64aef594f6aa
Davi de Oliveira <odavi278@gmail.com>
2023-12-20 00:49:13 -0300

    Ineferência de tipos

---
+++ InferênciaTipos/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Inferência de tipos");
+
+var nome = "Maria";
+var idade = 25;
+var salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} e ganha {salario.ToString("c")}");
+
+Console.ReadKey();
