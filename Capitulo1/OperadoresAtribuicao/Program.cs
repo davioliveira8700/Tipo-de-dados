d02a91c56bf80a4c7d0792d7bd578c4550a46ecd
Davi de Oliveira <odavi278@gmail.com>
2024-01-04 19:39:54 -0300

    Iniciando capitulo 2

---
+++ Capitulo1/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Opedores de Atribuição");
+
+Console.WriteLine("Infome a temperatura: \n");
+var temp = Convert.ToDouble(Console.ReadLine());
+
+var resultado = temp > 27 ? "Quente" : "Normal";
+
+Console.WriteLine($"O tempo está {resultado}");
+
+
+
+
+Console.ReadKey();
