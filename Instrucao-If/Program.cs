9079fbfe465a835a3c0ee6e53ed47e8b0443999c
Davi de Oliveira <odavi278@gmail.com>
2024-01-05 11:08:58 -0300

    Estruras condicionais

--- Instrucao-If/Program.cs
+++ Instrucao-If/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite primeiro numero: ");
+var num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite segundo numero: ");
+var num2 = int.Parse(Console.ReadLine());   
+if(num1 > num2)
+    Console.WriteLine("Numero 1 é maior que numero 2");
+if (num1 < num2)
+    Console.WriteLine("Numero 2 é maior que o numero 1 ");
+if (num1 == num2)
+    Console.WriteLine("Os dois numeros são iguais");
+
+Console.ReadKey();
