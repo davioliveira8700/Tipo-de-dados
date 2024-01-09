95fa4f511d20cd1b807ea980a0abe508e88f680d
Davi de Oliveira <odavi278@gmail.com>
2024-01-09 13:53:17 -0300

    Instrucao switch outro exemplo

---
+++ Switch2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Switch-case");
+
+Console.WriteLine("Informe o nome do mês");
+var mes = Console.ReadLine().ToLower();
+
+switch (mes)
+{
+    case "janeiro":
+    case "março":
+    case "maior":
+    case "julho":
+    case "agosto":
+    case "otubro":
+    case "dezembro":
+        Console.WriteLine("Este mês tem 31 dias");
+        break;
+    case "fevereiro":
+        Console.WriteLine("Este mês tem 28 ou 29 dias");
+        break;
+    default:
+        Console.WriteLine("Este mês tem 30 dias");
+        break;
+}
+Console.ReadKey();
