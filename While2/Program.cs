74ac80b78481b8f6012c3d7c09862e0722c7c451
Davi de Oliveira <odavi278@gmail.com>
2024-01-09 17:18:51 -0300

    While

---
+++ While2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Instrução While");
+
+int x = 0;
+while (x < 5) ;
+{
+    int y = 0;
+    while (y < 5)
+    {
+        Console.WriteLine($"{x},{y}");
+        y++;
+    }
+    x++;
+    Console.WriteLine();
+}
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
