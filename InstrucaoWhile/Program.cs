546e186502074ffe6202707c63c21f5d82328367
Davi de Oliveira <odavi278@gmail.com>
2024-01-09 15:57:21 -0300

    Instrucao while parte 1

---
+++ InstrucaoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("## WHILE ##");
+
+
+Console.WriteLine("Qual tabuada você quer obter? ");
+var valor = Convert.ToInt32(Console.ReadLine());
+var i = 1;
+if(valor > 0)
+{
+
+    while(i < 11)
+    {
+        Console.WriteLine($"{valor} X  {i} = {valor* i}");
+        i++;
+    }
+
+}
+else
+{
+    Console.WriteLine("Opção invalida");
+}
+Console.ReadKey();
