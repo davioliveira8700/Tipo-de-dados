d558164b082ad713047d32639c052513e6bcf23c
Davi de Oliveira <odavi278@gmail.com>
2024-01-09 15:27:52 -0300

    Instrucao GoTo

---
+++ GoTo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GoTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("Digite até qua numero você quer contar: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            
+        repetir:
+            Console.WriteLine($"i = {i}");
+            i = +i + 2;
+            
+            if(i <= valor)
+            {
+                goto repetir;
+                
+                
+            }
+            Console.WriteLine("Fim do processamento..");
+        }
+    }
+}
