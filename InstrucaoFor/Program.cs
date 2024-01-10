d3abac5e39f8293c2a8e764b9e2c75c5ed97f620
Davi de Oliveira <odavi278@gmail.com>
2024-01-10 16:53:50 -0300

    Instrucao fo

---
+++ InstrucaoFor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace InstrucaoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado, numero;
+            Console.WriteLine("Informe o numero :\t");
+            numero = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i<10; i++)
+            {
+                resultado = numero * i;
+                Console.WriteLine(numero + " X " + i + " = " + resultado);
+            }
+            Console.WriteLine("Fim do processamento...");
+
+        }
+    }
+}
--- While2/Program.cs
+++ While2/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Instrução While");
 
 int x = 0;
-while (x < 5) ;
+while (x < 5);
 {
     int y = 0;
     while (y < 5)
